ad50469a30dd12e7c113aad96cd50365aded171c
Seyed Ahmad Abtahi <seyedahmad.abtahi@gmail.com>
2022-07-14 18:13:19 +0430

    Solidity example files added

---
+++ Solidity/CH02/02_08/end/sample.sol
@@ -0,0 +1,28 @@
+// import solidity
+pragma solidity ^0.4.0;
+
+contract Simple {
+    // if statement
+    if (a == 2) {
+        // code if condition met
+    } else {
+        
+    }
+    
+    // checking condition first
+    while (a >= 0) {
+        // code if condition met
+    }
+    
+    // do run this code until condition no 
+    // longer true
+    do {
+        // do this code first
+    } while (a >= 0)
+    
+    // for loop
+    for (uint i=0; i>=50; i++) {
+        // code 
+    }
+    
+}
